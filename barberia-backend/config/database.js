5fa3680139238a7fe31a676291a2d409271c21c6
Mig.G <mig.garate@duocuc.cl>
2024-11-30 17:00:05 -0300

    arreglo de rutass

--- barberia-backend/config/database.js
+++ barberia-backend/config/database.js
@@ -1,13 +1,26 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config(); // Cargar las variables de entorno
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-  }
-);
+// Verificar si existe una URL de conexión directa
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'mysql',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false, // Deshabilitar validación de certificados para Railway
+        },
+      },
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
+        port: process.env.DB_PORT || 3306,
+      }
+    );
 
 module.exports = sequelize;
